270ceb616901346c897b81bf174487c162422bcc
Your Name <you@example.com>
2020-02-11 14:27:49 -0800

    final

--- server.py
+++ server.py
@@ -18,7 +18,15 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """
+    <!doctype html>
+    <html>
+    Hi! This is NOT the home page.
+    <a href ="http://localhost:5000/hello">LinkTexty</a>
+    </html>
+
+
+    """
 
 
 @app.route("/hello")
@@ -34,9 +42,20 @@ def say_hello():
       <body>
         <h1>Hi There!</h1>
         <form action="/greet">
-          What's your name? <input type="text" name="person">
+          What's your name? 
+          <input type="radio" name="person" value = "happy">
+          <label>Happy</label>
+          <input type="radio" name="person" value = "sad">
+          <label>Sad</label>
+          <input type="radio" name="person" value = "grumpy">
+          <label>Grumpy</label>
           <input type="submit" value="Submit">
         </form>
+        <form action = "/diss">
+          <input type="radio" name="person" value = "dopey">
+          <label>Dopey</label>
+           <input type="submit" value="Submit">
+        <form>
       </body>
     </html>
     """
@@ -50,7 +69,7 @@ def greet_person():
 
     compliment = choice(AWESOMENESS)
 
-    y = x
+
 
     return """
     <!doctype html>
@@ -65,7 +84,33 @@ def greet_person():
     """.format(player, compliment)
 
 
+@app.route("/diss")
+def diss_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+
+
+
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {}! I think you're dumb!
+      </body>
+    </html>
+    """.format(player)
+
+
+
+
+
+
 if __name__ == "__main__":
     # debug=True gives us error messages in the browser and also "reloads"
     # our web app if we change the code.
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
